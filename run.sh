78a3d7f87b808549c56b5b72663aca2a23703644
maestroi <meastro@protonmail.com>
2023-05-13 13:01:06 +0200

    add better script

--- run.sh
+++ run.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
 
-# Install docker
-sudo apt-get update
-sudo apt-get install -y docker.io git
+# Check if the script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root."
+   exit 1
+fi
 
-# Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# Create a new user called "nimiq" with home directory "/opt/nimiq"
+useradd -m -d /opt/nimiq nimiq
 
-# Create the nimiq user
-sudo adduser  --home /home/nimiq --disabled-password --gecos "" nimiq
+# Update the package repositories and install packages
+apt update
+apt install -y git docker.io docker-compose ufw fail2ban unattended-upgrades
 
-# Add the nimiq user to the docker group
-sudo usermod -aG docker nimiq
+# Add the "nimiq" user to the "docker" group
+usermod -aG docker nimiq
+
+# Enable UFW (Uncomplicated Firewall)
+ufw enable
+
+# Allow incoming connections on ports 80, 443, and 22
+ufw allow 80
+ufw allow 443
+ufw allow 22
+
+# Block all other incoming traffic
+ufw default deny
+
+# Create the /opt/nimiq directory
+mkdir -p /opt/nimiq
+
+# Set the ownership of the /opt/nimiq directory to the nimiq user
+chown -R nimiq:nimiq /opt/nimiq
 
 # Clone the NimiqPool repository
-sudo -u nimiq git clone https://github.com/maestroi/NimiqPool.git /home/nimiq/NimiqPool
+sudo -u nimiq git clone https://github.com/maestroi/NimiqPool.git /opt/nimiq
+
+# Create the necessary directories
+sudo -u nimiq mkdir /opt/nimiq/database
+sudo -u nimiq mkdir /opt/nimiq/worker
+sudo -u nimiq mkdir /opt/nimiq/master
+sudo -u nimiq mkdir /opt/nimiq/payout
+
+# Allow the "nimiq" user to run Docker commands without a password
+echo 'nimiq ALL=(ALL) NOPASSWD: /usr/bin/docker' | tee /etc/sudoers.d/nimiq-docker
+
+# Update the user's shell to Bash
+chsh -s /bin/bash nimiq
